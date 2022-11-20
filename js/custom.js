e3232434984f50bc95fb307821f61e89603c0f1d
Jairo <jgtrue@protonmail.com>
2022-11-20 18:37:22 +0100

    signIn-signUp

--- js/custom.js
+++ js/custom.js
@@ -23,3 +23,49 @@ function showContent(idDiv){
         selectedDiv.classList.add('visible');
     }
 }
+
+function swapLogRegister(idDiv){
+    let log = 'loginPane';
+    let reg = 'registerPane';
+    if(idDiv === reg){ 
+        reg = document.getElementById(reg);
+        log = document.getElementById(log);
+        if(!reg.classList.contains('show')){
+            log.classList.remove('show');
+            log.classList.remove('active');
+            reg.classList.add('show');
+            reg.classList.add('active');
+            
+        }
+    }else{
+        reg = document.getElementById(reg);
+        log = document.getElementById(log);
+        if(!log.classList.contains('show')) {
+            reg.classList.remove('show');
+            reg.classList.remove('active');
+            log.classList.add('show');
+            log.classList.add('active');
+        }     
+    }
+    
+}
+
+function showLog(idDiv, option){
+    let selectedDiv = idDiv;
+    if(selectedDiv === 'singPane')
+        selectedDiv = document.getElementById(idDiv);
+        if(selectedDiv.classList.contains('no-visible')){
+            selectedDiv.classList.remove('no-visible');
+            selectedDiv.classList.add('visible');
+            (option === 1) ? swapLogRegister('loginPane') :  swapLogRegister('registerPane');
+           
+        }
+}
+
+function closePane(idDiv){
+    idDiv = document.getElementById(idDiv);
+    if(idDiv.classList.contains('visible')){
+        idDiv.classList.remove('visible');
+        idDiv.classList.add('no-visible');
+    }
+}
